e51cfa5033cf676c9ada55db02b9aa3e35bb34ca
Hangsihak Sin <hsin9104@conestogac.on.ca>
2024-03-25 00:04:24 -0400

    add: implement Camera screen with basic components
    Co-Authored-By: mgordon <146022076+mag9073@users.noreply.github.com>

--- app/src/main/java/com/example/home_security_system_app/MainActivity.java
+++ app/src/main/java/com/example/home_security_system_app/MainActivity.java
@@ -1,24 +1,33 @@
 package com.example.home_security_system_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView cameraCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView((R.layout.activity_main));
+
+        cameraCard = findViewById(R.id.cameraCard);
+
+        cameraCard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
