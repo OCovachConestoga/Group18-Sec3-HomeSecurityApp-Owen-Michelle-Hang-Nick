5e9369d1c38311d3327630ef84a621525ef0d39a
Michelle Gordon <Mgordon9073@conestogac.on.ca>
2024-03-27 19:16:05 -0400

    updated light class with activity_light.xml and png

--- app/src/main/java/com/example/home_security_system_app/MainActivity.java
+++ app/src/main/java/com/example/home_security_system_app/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     CardView cameraCard;
+    CardView lightCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView((R.layout.activity_main));
 
         cameraCard = findViewById(R.id.cameraCard);
+        lightCard = findViewById(R.id.lightCard);
 
         cameraCard.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -29,5 +31,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        lightCard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LightActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
