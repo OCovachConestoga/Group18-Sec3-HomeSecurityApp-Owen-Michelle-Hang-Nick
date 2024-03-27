3fd37a95c966585ee473455b7289d8b0a3198a62
Michelle Gordon <Mgordon9073@conestogac.on.ca>
2024-03-27 17:35:00 -0400

    updated light class.

--- app/src/main/java/com/example/home_security_system_app/LightActivity.java
+++ app/src/main/java/com/example/home_security_system_app/LightActivity.java
@@ -1,5 +1,24 @@
 package com.example.home_security_system_app;
 
-public class LightActivity {
-}
+import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_light);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
