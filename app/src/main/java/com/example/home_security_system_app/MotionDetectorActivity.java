a5f69d6de81f4c55ab9450112e27bf84fca108e9
CovOwe04 <owenacov@gmail.com>
2024-03-27 16:55:44 -0400

    Added logic for setup of DoorLocks and MotionDetector

--- app/src/main/java/com/example/home_security_system_app/MotionDetectorActivity.java
+++ app/src/main/java/com/example/home_security_system_app/MotionDetectorActivity.java
@@ -2,6 +2,7 @@ package com.example.home_security_system_app;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,28 +10,56 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MotionDetectorActivity extends AppCompatActivity {
 
     private TextView motionDetectorDisplay;
+    private Button tempSetupButton;
+
+    private boolean isSetup = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_doorlocks);
+        setContentView(R.layout.activity_motiondetector);
 
+        motionDetectorDisplay = findViewById(R.id.motionDetectorView);
+        motionDetectorDisplay.setText("No Motion Detector Setup /o\\");
     }
 
     public void buttonSetup(View view){
+        tempSetupButton = findViewById(R.id.setupButton);
         motionDetectorDisplay = findViewById(R.id.motionDetectorView);
-        motionDetectorDisplay.setText("Motion Detector Setup \\o/");
+
+        String btnText = tempSetupButton.getText().toString();
+
+        if(btnText.equals("Setup"))
+        {
+            tempSetupButton.setText("Disconnect");
+            motionDetectorDisplay.setText("Motion Detector Setup \\o/");
+        }
+        else
+        {
+            tempSetupButton.setText("Setup");
+            motionDetectorDisplay.setText("Motion Detector Disconnected \\o/");
+        }
+
+        isSetup = !isSetup;
     }
 
     public void buttonStartDetecting(View view){
         motionDetectorDisplay = findViewById(R.id.motionDetectorView);
-        motionDetectorDisplay.setText("Started Detecting \\o/");
+
+        if(isSetup)
+            motionDetectorDisplay.setText("Started Detecting \\o/");
+        else
+            motionDetectorDisplay.setText("No Motion Detector to Start /o\\");
     }
 
     public void buttonStopDetecting(View view){
         motionDetectorDisplay = findViewById(R.id.motionDetectorView);
-        motionDetectorDisplay.setText("Stopped Detecting \\o/");
+
+        if(isSetup)
+            motionDetectorDisplay.setText("Stopped Detecting \\o/");
+        else
+            motionDetectorDisplay.setText("No Motion Detector to Stop /o\\");
     }
 
 }
